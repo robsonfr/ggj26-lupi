07c693b30aa40791287ce835f57f1043a9d9acce
Robson França <robson@morot.com.br>
2026-01-31 17:18:42 -0300

    Oito direcoes

--- aster.lua
+++ aster.lua
@@ -6,16 +6,16 @@ Aster = {
     y = 0,
     estado=1,
     draw = function(self, m)
-        if estado < 5 then
+        if self.estado < 5 then
             l = m:paralocal(self.x, self.y)
             if l.natela then
-                ui.spr(Sprites["aster0" .. estado], l.x, l.y)
+                ui.spr(Sprites["aster0" .. self.estado], l.x, l.y)
             end
         end
     end,
     atingiu = function(self)
-        if estado < 5 then
-            estado = estado + 1
+        if self.estado < 5 then
+            self.estado = self.estado + 1
         end
     end
 }
--- game.lua
+++ game.lua
@@ -2,140 +2,111 @@ require "palette"
 require "sprites"
 require "aster"
 require "mundo"
--- function _draw()
---     ui.cls(1)
---     ui.print("Teste Global Game Jam 2026", 80, 120, 7)
--- end 
 
-x = 230
-y = 140
-
-asteroides = {}
-for i = 1, 10 do
-    asteroides[i] = Aster:new()
-    asteroides[i].x = math.random(-400,400)
-    asteroides[i].y = math.random(-320,320)
-    asteroides[i].estado = math.random(1,4)
+Asteroides = {}
+for i = 1, 100 do
+    Asteroides[i] = Aster:new()
+    Asteroides[i].x = math.random(-400,400)
+    Asteroides[i].y = math.random(-320,320)
+    Asteroides[i].estado = math.random(1,4)
 end
 
+Mm = Mundo:new()
 
-mm = Mundo:new()
-a_x = 100
-a_y = 100
+Step = 1
+Substep = 0
 
-step = 1
-substep = 0
+Direcao = 1
 
-estado = 1
-direcao = 1
 
 function update()
     local ajuste
     ajuste = 0
+    DirX = 0
+    DirY = 0
     if ui.btn(LEFT) then
-        direcao = 4
+        --Direcao = 4
+        DirX = -1
         ajuste = 1
     end
     if ui.btn(RIGHT) then
-        direcao = 2
+        -- Direcao = 2
+        DirX = 1
         ajuste = 1
     end
     if ui.btn(UP) then
-        direcao = 1
+        --Direcao = 1
+        DirY = -1
         ajuste = 1
     end
     if ui.btn(DOWN) then
-        direcao = 3
+        -- Direcao = 3
+        DirY = 1
         ajuste = 1
     end
 
-    if ajuste == 1 then
-        if direcao == 1 then
-            mm:move_y(-step)
-        elseif direcao == 2 then
-            mm:move_x(step)
-        elseif direcao == 3 then
-            mm:move_y(step)
-        elseif direcao == 4 then
-            mm:move_x(-step)
+    if DirX == 0 and DirY ~= 0 then
+        if DirY == -1 then
+            Direcao = 1
+        elseif DirY == 1 then
+            Direcao = 3
         end
-        substep = substep + 1
-        if substep >= 10 then
-            step = step + 1
-            substep = 0
+    elseif DirY == 0 and DirX ~= 0 then
+        if DirX == 1 then
+            Direcao = 2
+        elseif DirX == -1 then
+            Direcao = 4
         end
     else
-        substep = 0
-        step = 1
+        if DirX == 1 and DirY == -1 then
+            Direcao = 5
+        elseif DirX == 1 and DirY == 1 then
+            Direcao = 6
+        elseif DirX == -1 and DirY == 1 then
+            Direcao = 7
+        elseif DirX == -1 and DirY == -1 then
+            Direcao = 8
+        end
     end
 
-    -- if ui.btn(LEFT) and (x > step) then
-    --     x = x - step
-    --     ajuste = 1
-    -- end
-    -- if ui.btn(RIGHT) and (x < 460-step) then
-    --     x = x + step
-    --     ajuste = 1
-    -- end
-
-    -- if ui.btn(UP) and (y > step) then
-    --     y = y - step
-    --     ajuste = 1
-    -- end
-    -- if ui.btn(DOWN) and (y < 250-step) then
-    --     y = y + step
-    --     ajuste = 1
-    -- end
-    -- if ajuste == 1 then
-    --     substep = substep + 1
-    --     if substep >= 10 then
-    --         step = step + 1
-    --         substep = 0
-    --     end
-    -- else
-    --         step = 1
-    --         substep = 0
-
-    -- end
-
+    if ajuste == 1 then
+        if Direcao == 1 or Direcao == 5 or Direcao == 8 then
+            Mm:move_y(-Step)
+        elseif Direcao == 2 or Direcao == 5 or Direcao == 6 then
+            Mm:move_x(Step)
+        elseif Direcao == 3 or Direcao == 6 or Direcao == 7 then
+            Mm:move_y(Step)
+        elseif Direcao == 4 or Direcao == 7 or Direcao == 8 then
+            Mm:move_x(-Step)
+        end
+        Substep = Substep + 1
+        if Substep >= 10 then
+            Step = Step + 1
+            Substep = 0
+        end
+    else
+        Substep = 0
+        Step = 1
+    end
 
     for i = 1, #Palette do
         ui.palset(i-1, Palette[i])
     end
-    -- ui.palset(0, 0x0)
-    -- ui.palset(1, 0xBF8)
-    -- ui.palset(2, 0x6D1)
-    -- ui.palset(3, 0x64E)
-    -- ui.palset(4, 0xB9F)
-    -- ui.palset(5, 0x6B7)
-    -- ui.palset(6, 0x612)
-    -- ui.palset(7, 0x7D42)
-    -- ui.palset(8, 0x60E1)
-    -- ui.palset(9, 0x40A1)
-    -- ui.palset(10, 0x1C5F)
-    -- ui.palset(11, 0x1838)
-    -- ui.palset(12, 0x1034)
-    -- ui.palset(13, 0x6C5F)
-    -- ui.palset(14, 0x4835)
-    -- ui.palset(15, 0x7FFF)
-    if ui.btnp(BTN_Z) then
-        mm:zero()
-    end
-    -- if ui.btnp(BTN_Z) and estado < 4 then
-    --     estado = estado + 1
-    -- end
 
-    -- if ui.btnp(BTN_X) and estado > 1 then
-    --     estado = estado - 1
-    -- end
+    if ui.btnp(BTN_Z) then
+        Mm:zero()
+    end
 
     ui.cls(0)
     ui.print("Monstrao Mascarado", 200, 260, 2)
-    ui.spr(Sprites["nave0" .. direcao], 232, 127)
-    for i = 1, 10 do
-        asteroides[i]:draw(mm)
+    ui.spr(Sprites["nave0" .. Direcao], 232, 127)
+    
+    for i = 1, #Asteroides do
+        if Asteroides[i].x >= -16 and Asteroides[i].x <= 16 and Asteroides[i].y >= -16 and Asteroides[i].y <= 16 then
+            ui.print("Game over", 200, 220, 2)
+        end
+        Asteroides[i]:draw(Mm)
     end
-    -- ui.spr(Sprites["aster0" .. estado], a_x, a_y)
     ui.spr(Sprites["mask01"], 160, 100)
     ui.spr(Sprites["mask01"], 360, 100)
 end
