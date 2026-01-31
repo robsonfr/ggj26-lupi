a5ea441807373379ea6d2e99a6c3d54ff70e2936
Robson França <robson@morot.com.br>
2026-01-30 21:18:08 -0300

    Hotfix

--- game.lua
+++ game.lua
@@ -1,9 +1,13 @@
-function _draw()
+-- function _draw()
+--     ui.cls(1)
+--     ui.print("Teste Global Game Jam 2026", 80, 120, 7)
+-- end 
+
+function update()
+    ui.palset(0, 0x03E6) 
+    ui.palset(1, 0x07E0) 
+    ui.palset(2, 0x467A)
     ui.cls(1)
     ui.print("Teste Global Game Jam 2026", 80, 120, 7)
-end 
 
-function _update()
-    if ui.btn(0) then
-    end
 end
